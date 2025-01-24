ed8967ce998a01ca21cf65a2a8a7e3f55f9a0303
Daniela Araujo <daniela.scaraujo@outlook.com>
2025-01-24 14:50:44 -0300

    First commit

---
+++ src/Media.java
@@ -0,0 +1,13 @@
+public class Media {
+    public static void main (String[] args) {
+        System.out.println("Cálculo da média");
+        double nota1 = 5;
+        double nota2 = 7.5;
+        double nota3 = 8.3;
+        double nota4 = 9.5;
+
+        double media = (nota1 + nota2 + nota3 + nota4) / 4;
+
+        System.out.println(("Sua média é:" + media));
+    }
+}
